3effba0d60c334e48a0778421680542afccfa041
michxas <100387425+michxas@users.noreply.github.com>
2022-03-26 03:17:12 +0100

    bro

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.4;
+  audio.play()
+}
+
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.play()
+}
+
---
+++ typed.js
@@ -0,0 +1,9 @@
+
+
+new Typed('#typed',{
+    strings : ['michas','Bid Dev with Big Namer "Neksio"','skid/nerd','python copy paster','simp4zosia','GitHub Enjoyer'],
+    typeSpeed : 40,
+    delaySpeed : 40,
+    loop : true
+  });
+
